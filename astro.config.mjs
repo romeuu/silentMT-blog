ebfda1a02fc7db4e9a29a15642e3858596fc3511
Sergio Romeu <sergioromagr@gmail.com>
2025-02-27 19:59:40 +0100

    fix: vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config'; // import lit from '@astrojs/lit';
-
-import sitemap from '@astrojs/sitemap';
-
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-minimal-starter.netlify.app/",
   integrations: [sitemap()],
+  output: 'static',
+  adapter: vercel(),
   build: {
     format: 'directory'
   }
